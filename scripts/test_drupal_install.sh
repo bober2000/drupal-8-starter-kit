7be9c3de188eb04f2823e1cf4856b3aaaa1d4dee
Vitalii Morvaniuk <bober2000@ukr.net>
2016-07-14 16:54:29 +0300

    Fix bootsrap path

--- scripts/test_drupal_install.sh
+++ scripts/test_drupal_install.sh
@@ -18,6 +18,6 @@ sudo composer self-update
 
 echo "Installing $CORE to " . $DOCROOT
 
-drush site-install standard -y --root=$DOCROOT --account-mail=$ACCOUNT_MAIL --account-name=$ACCOUNT_USER --account-pass="$ACCOUNT_PASS" --site-mail=$SITE_MAIL --site-name="$SITE_NAME" --uri=http://$DOMAIN_NAME --db-url=mysql://$DATABASE_USER:$DATABASE_PASS@localhost/$DATABASE_NAME
+drush site-install standard -y --root=$DOCROOT --account-mail=$ACCOUNT_MAIL --account-name=$ACCOUNT_USER --account-pass="$ACCOUNT_PASS" --site-mail=$SITE_MAIL --site-name="$SITE_NAME" --uri=http://localhost --db-url=mysql://$DATABASE_USER:$DATABASE_PASS@localhost/$DATABASE_NAME
 
 echo "user: $ACCOUNT_USER pass: $ACCOUNT_PASS"
